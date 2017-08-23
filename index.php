d11bdce5b593f458c39dab7ed5d98c4fc00a73eb
Ramil Rakhmatullin <rep555333@gmail.com>
2017-08-23 21:45:55 +0300

    выполнил второе задание

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
 // показывать или нет выполненные задачи
 $show_complete_tasks = rand(0, 1);
-
 // устанавливаем часовой пояс в Московское время
 date_default_timezone_set('Europe/Moscow');
 
@@ -119,15 +118,29 @@ $days_until_deadline = null;
                             <span class="radio-button__text">Просроченные</span>
                         </label>
                     </div>
-
                     <label class="checkbox">
                         <!--добавить сюда аттрибут "checked", если переменная $show_complete_tasks равна единице-->
-                        <input id="show-complete-tasks" class="checkbox__input visually-hidden" type="checkbox">
+                        <input id="show-complete-tasks" class="checkbox__input visually-hidden" type="checkbox"
+                          <?php if($show_complete_tasks == 1): ?>checked<?php endif; ?>>
                         <span class="checkbox__text">Показывать выполненные</span>
                     </label>
                 </div>
 
                 <table class="tasks">
+                    <?php if($show_complete_tasks == 1): ?>
+                      <tr class="tasks__item task task--completed">
+                        <td class="task__select">
+                          <label class="checkbox task__checkbox">
+                            <input class="checkbox__input visually-hidden" type="checkbox" checked>
+                            <span class="checkbox__text">Записаться на интенсив "Базовый PHP"</span>
+                          </label>
+                        </td>
+                        <td class="task__date">10.04.2017</td>
+
+                        <td class="task__controls">
+                        </td>
+                      </tr>
+                    <?php endif; ?>
 
                     <!--показывать следующий тег <tr/>, если переменная равна единице-->
                     <tr class="tasks__item task task--completed">
