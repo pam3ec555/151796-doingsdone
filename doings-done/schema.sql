9a6643c59b97e66cfa75f045bbae443a2d520efe
Ramil Rakhmatullin <rep555333@gmail.com>
2017-09-09 20:33:16 +0300

    added mysql code to schema.sql

--- doings-done/schema.sql
+++ doings-done/schema.sql
@@ -1,1 +1,30 @@
-SHOW DATABASES;
+CREATE DATABASE doingsdone;
+
+USE doingsdone;
+
+CREATE TABLE projects (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  project CHAR(64)
+);
+
+CREATE TABLE tasks (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  date_create DATETIME,
+  date_complete DATETIME,
+  name CHAR(64),
+  file CHAR,
+  deadline DATETIME,
+  project INT,
+  author INT
+);
+
+CREATE TABLE users (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  date_of_registration DATETIME,
+  email CHAR(32) UNIQUE ,
+  name CHAR(32),
+  password CHAR(64),
+  contacts CHAR,
+  tasks TEXT,
+  projects TEXT
+);
